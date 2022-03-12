0e422d91ba7298ff34aba567a32615bedb4bf298
stephenasuncionDEV <stephenasuncionDEV@users.noreply.github.com>
2022-03-12 09:29:47 +0000

    Deploying to gh-pages from  @ 2d388a7b324233de57f0d2d627f48a5bdbbd5198 🚀

---
+++ _next/static/Q-vGXYvQGwwUHvO0YHXIV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-2c7c3d2f4dfa133e.js","static/chunks/507-17e9d145e5c1512c.js","static/chunks/pages/index-0b471f40aa31d04c.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
