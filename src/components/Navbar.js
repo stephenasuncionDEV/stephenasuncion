52a012763d0590f09fc3cfc74effe05e8d35146a
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-08-07 22:53:11 -0700

    🌻UPDATE: changed to navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { Flex, HStack, Button, IconButton, Image, useColorMode,
     MenuButton, MenuList, Menu, MenuItem, MenuDivider
  } from '@chakra-ui/react'
@@ -10,47 +11,53 @@ import { BsPerson, BsFolder, BsFillHeartFill } from 'react-icons/bs'
 import { HiOutlineDocumentText } from 'react-icons/hi'
 
 const Navbar = () => {
+    const router = useRouter();
     const { colorMode, toggleColorMode } = useColorMode();
     const isTouchingLogo = useMediaQuery({ query: '(max-width: 530px)' });
-    
+    const route = router.route;
+
     return (
         <Flex p='1em' justifyContent='space-between'>
             <Link href='/' passHref>
                 <Image src='/assets/bitmoji.webp' alt='Stephen Asuncion Avatar' width='50px' fallbackSrc='https://picsum.photos/50' cursor='pointer'/>
             </Link>
             {!isTouchingLogo ? (
-                <HStack>
-                    <Link href='/' shallow passHref style={{ textDecoration: 'none' }}>
-                        <Button size='sm' variant='transparent'>
-                            Home
-                        </Button>
-                    </Link>
-                    <Link href='/about' shallow passHref style={{ textDecoration: 'none' }}>
-                        <Button size='sm' variant='transparent'>
-                            About
-                        </Button>
-                    </Link>
-                    <Link href='/projects' shallow passHref style={{ textDecoration: 'none' }}>
-                        <Button size='sm' variant='transparent'>
-                            Projects
-                        </Button>
-                    </Link>
-                    <Link href='/blog' shallow passHref style={{ textDecoration: 'none' }}>
-                        <Button size='sm' variant='transparent'>
-                            Blog
-                        </Button>
-                    </Link>
-                    <Link href='mailto:stephenasuncion@outlook.com' style={{ textDecoration: 'none' }}>
-                        <Button size='sm' variant='primary'>
-                            🚀 Hire Me
-                        </Button>
-                    </Link>
-                    <IconButton 
-                        aria-label='Toggle Color Mode' 
-                        icon={colorMode === 'light' ? <MdOutlineDarkMode /> : <MdOutlineLightMode />} 
-                        variant='transparent'
-                        onClick={toggleColorMode}
-                    />
+                <HStack spacing='2em'>
+                    <HStack spacing='1em'>
+                        <Link href='/' shallow passHref style={{ textDecoration: 'none' }}>
+                            <Button size='sm' variant='transparent' borderBottom={route === '/' ? '2px dashed rgb(52,140,212)' : 'initial'} borderRadius='0'>
+                                Home
+                            </Button>
+                        </Link>
+                        <Link href='/about' shallow passHref style={{ textDecoration: 'none' }}>
+                            <Button size='sm' variant='transparent' borderBottom={route === '/about' ? '2px dashed rgb(52,140,212)' : 'initial'} borderRadius='0'>
+                                About
+                            </Button>
+                        </Link>
+                        <Link href='/projects' shallow passHref style={{ textDecoration: 'none' }}>
+                            <Button size='sm' variant='transparent' borderBottom={route === '/projects' ? '2px dashed rgb(52,140,212)' : 'initial'} borderRadius='0'>
+                                Projects
+                            </Button>
+                        </Link>
+                        <Link href='/blog' shallow passHref style={{ textDecoration: 'none' }}>
+                            <Button size='sm' variant='transparent' borderBottom={route === '/blog' ? '2px dashed rgb(52,140,212)' : 'initial'} borderRadius='0'>
+                                Blog
+                            </Button>
+                        </Link>
+                    </HStack>
+                    <HStack>
+                        <Link href='mailto:stephenasuncion@outlook.com' style={{ textDecoration: 'none' }}>
+                            <Button size='sm' variant='primary'>
+                                🚀 Hire Me
+                            </Button>
+                        </Link>
+                        <IconButton 
+                            aria-label='Toggle Color Mode' 
+                            icon={colorMode === 'light' ? <MdOutlineDarkMode /> : <MdOutlineLightMode />} 
+                            variant='transparent'
+                            onClick={toggleColorMode}
+                        />
+                    </HStack>
                 </HStack>
             ) : (
                 <Menu>
