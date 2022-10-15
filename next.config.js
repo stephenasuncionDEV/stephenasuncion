8164c21ea6dce902f677967f87a76635f7f631b3
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-10-14 18:31:08 -0700

    🔥 REMOVE: remobed github page code

--- next.config.js
+++ next.config.js
@@ -1,36 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+
+const nextConfig = withBundleAnalyzer({
+    reactStrictMode: false,
+    swcMinify: true,
     env: {
-        SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
-        SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
-        SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN,
-        GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+        GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
         GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-        GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN
+        GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+        SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN,
+        SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+        SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     },
     images: {
-        path: '/',
-    },
-    exportPathMap: async (defaultPathMap, { dev, dir, outDir, distDir, buildId }) => {
-        return {
-            '/': { page: '/' },
-        }
-    },
-    async headers() { // From @FDisk https://stackoverflow.com/questions/62077589/setting-cache-control-header-for-static-file-serving-on-nextjs-default-server
-        return [
-            {
-                source: '/:all*(svg|jpg|png)',
-                locale: false,
-                headers: [
-                {
-                    key: 'Cache-Control',
-                    value: 'public, max-age=9999999999, must-revalidate',
-                }
-                ],
-            },
-        ]
-    },
-}
+        domains: [''],
+    }
+})
 
 module.exports = nextConfig
