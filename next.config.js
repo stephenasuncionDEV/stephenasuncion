550c022f50c693dbe75e5fdf13e8e2911178fa9d
Stephen Asuncion <stephenasuncion@outlook.com>
2022-10-24 22:46:36 -0800

    🚀 UPDATE: updated image path

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-})
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig = withBundleAnalyzer({
-    reactStrictMode: false,
-    swcMinify: true,
-    env: {
-        GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
-        GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-        GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-        SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN,
-        SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
-        SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
-    },
-    images: {
-        domains: [''],
-    }
-})
+  reactStrictMode: false,
+  swcMinify: true,
+  env: {
+    GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+  },
+  images: {
+    path: "/",
+  },
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
