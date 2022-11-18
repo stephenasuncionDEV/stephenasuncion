b20bef57095f9446d9c426c18901001a6c64f939
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-11-17 21:58:02 -0800

    🔥 REMOVE: removed backslash front clienturl

--- src/config/prod.ts
+++ src/config/prod.ts
@@ -2,7 +2,7 @@ import { SingleConfig } from "@/config/index";
 
 const production: SingleConfig = {
   serverURL: "http://localhost:8080",
-  clientURL: "https://stephenasuncion.dev/",
+  clientURL: "https://stephenasuncion.dev",
 };
 
 export default production;
